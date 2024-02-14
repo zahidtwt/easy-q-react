5bb80060f9543f1d3c6656b218282afcd32562ce
Sk. Zaber Ahmed <zaber0305@gmail.com>
2024-02-14 16:14:09 +0600

    Setup(:x): Added required setup files and modified some configuration files Added husky for commit messasge, pre-commit and pre-push hooks, added vscode workspace settings, added prettier config and prettier ignore files and editorconfig according to prettier rules, modified and extended some eslint config options, rules and added eslint ignore files, added npmrc for strict npm versioning and nvmrc for node versioning, added commit lint config options and reformatted tailwind config to ts version

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,37 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
   },
-}
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs", "src/**/*.test.ts"],
+  plugins: ["react-refresh", "@typescript-eslint", "react-hooks"],
+  globals: {
+    React: "readonly",
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  rules: {
+    "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+    "prettier/prettier": ["error"],
+    "no-console": ["error", { allow: ["warn", "error"] }],
+    "no-irregular-whitespace": "warn",
+    "no-self-assign": "error",
+    "no-self-compare": "error",
+    "no-unneeded-ternary": "warn",
+  },
+};
---
+++ commitlint.config.ts
@@ -0,0 +1,83 @@
+import type { UserConfig } from "@commitlint/types";
+import { RuleConfigSeverity } from "@commitlint/types";
+
+const configuration: UserConfig = {
+  extends: ["@commitlint/config-conventional"],
+
+  rules: {
+    "body-leading-blank": [RuleConfigSeverity.Disabled, "always"],
+    "body-max-line-length": [RuleConfigSeverity.Error, "always", 100],
+    "footer-leading-blank": [RuleConfigSeverity.Warning, "always"],
+    "footer-max-line-length": [RuleConfigSeverity.Error, "always", 100],
+    "header-max-length": [RuleConfigSeverity.Error, "always", 100],
+    "scope-case": [RuleConfigSeverity.Error, "always", "lower-case"],
+    "scope-empty": [RuleConfigSeverity.Disabled, "always"],
+    "subject-case": [RuleConfigSeverity.Error, "never", ["lower-case", "start-case", "pascal-case", "upper-case"]],
+    "subject-empty": [RuleConfigSeverity.Error, "never"],
+    "subject-full-stop": [RuleConfigSeverity.Error, "never", "."],
+    "type-case": [RuleConfigSeverity.Error, "always", "sentence-case"],
+    "type-empty": [RuleConfigSeverity.Error, "never"],
+    "type-enum": [
+      RuleConfigSeverity.Error,
+      "always",
+      ["Build", "Feat", "Fix", "Merge", "Modify", "Optimize", "Refactor", "Setup"],
+    ],
+  },
+  prompt: {
+    messages: {
+      skip: ":x",
+      max: "upper %d characters",
+      min: "%d characters at least",
+      emptyWarning: "can not be empty",
+      upperLimitWarning: "is going above limit",
+      lowerLimitWarning: "is going below limit",
+    },
+    questions: {
+      type: {
+        description: "Select the type of change that you're committing:",
+        enum: {
+          Feat: {
+            description: "Introduce a new capability or feature.",
+            title: "Feat",
+            emoji: "✨",
+          },
+          Modify: {
+            description: "Modify or enhance an existing capability, configuration, or rule.",
+            title: "Modify",
+            emoji: "🚀",
+          },
+
+          Fix: {
+            description: "Correct an issue, such as a bug, typo, or misstatement.",
+            title: "Fix",
+            emoji: "🐛",
+          },
+          Build: {
+            description: "Change the build process, tooling, or infrastructure.",
+            title: "Build",
+            emoji: "🛠",
+          },
+          Refactor: {
+            description: "Restructure the code without changing its external behavior.",
+            title: "Refactor",
+            emoji: "📦",
+          },
+          Optimize: {
+            description: "Enhance performance, speeding up code or processes.",
+            title: "Optimize",
+            emoji: "⚙️",
+          },
+          Setup: {
+            description: "Setting up configuration files and rules",
+            title: "Setup",
+            emoji: "📚",
+          },
+        },
+      },
+      subject: {
+        description: "Write a short and concise description of the change",
+      },
+    },
+  },
+};
+module.exports = configuration;
