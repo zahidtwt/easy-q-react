b6d746a3a8b669a3f5eef3c7d0e95f8cccccd078
Sk. Zaber Ahmed <zaber0305@gmail.com>
2024-02-22 21:22:46 +0600

    Modify: rewrote profile basic structure with shadcn card

--- src/pages/dashboard/layout.tsx
+++ src/pages/dashboard/layout.tsx
@@ -4,7 +4,7 @@ import { Outlet } from "react-router-dom";
 const DashboardLayout = () => {
   return (
     <div>
-      <div className="container">
+      <div className="container pb-[60px]">
         <Outlet />
       </div>
       <NavBar />
--- src/pages/dashboard/profile/profile.tsx
+++ src/pages/dashboard/profile/profile.tsx
@@ -1,49 +1,74 @@
 import imagePlaceholder from "@/assets/image-dummy.svg";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Edit } from "lucide-react";
 
 const Profile = () => {
   return (
     <div className=" py-2 text-center space-y-8 overflow-x-scroll">
-      <div className="flex flex-col justify-center items-center gap-2 shadow-lg rounded-md p-2 relative">
-        <Edit className="absolute top-1 right-1" />
-        <img
-          src={imagePlaceholder}
-          alt={"Image placeholder"}
-          className="rounded-full size-16"
-        />
+      <Card>
+        <CardContent className="flex flex-col justify-center items-center gap-2 shadow-lg rounded-md p-2 relative">
+          <Edit className="absolute top-1 right-1" />
+          <Avatar className="border border-slate-500 size-16">
+            <AvatarImage src={imagePlaceholder} />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
 
-        <div>
-          <p>Jamia Islamia Rowjatul Ulum Madrasa</p>
-          <small>Hat Govindpur, Faridpur</small>
-        </div>
+          <div>
+            <p>Jamia Islamia Rowjatul Ulum Madrasa</p>
+            <small>Hat Govindpur, Faridpur</small>
+          </div>
 
-        <div>
-          <p className="block">
-            Mobile: <span>016XXXXX541</span>
+          <CardFooter>
+            <div>
+              <p className="block">
+                Mobile: <span>016XXXXX541</span>
+              </p>
+              <p className="block">
+                Email: <span>zaberXXXXX@gmail.com</span>
+              </p>
+            </div>
+          </CardFooter>
+        </CardContent>
+      </Card>
+
+      <Card className="flex flex-col justify-center items-center gap-2 shadow-lg rounded-md">
+        <CardHeader>
+          <CardTitle className="border border-gray-400 rounded-md py-1 px-2 bg-slate-100 text-md font-medium">
+            Education board
+          </CardTitle>
+        </CardHeader>
+        <CardContent className="space-y-4">
+          <p className="border border-gray-400 py-1 px-2 text-sm rounded-sm bg-slate-300">
+            Nurani Education Board, Chittagong
           </p>
-          <p className="block">
-            Email: <span>zaberXXXXX@gmail.com</span>
+          <p className="border border-gray-400 py-1 px-2 text-sm rounded-sm bg-slate-300">
+            Nurani Education Board, Rangpur
           </p>
-        </div>
-      </div>
-      <div className="flex flex-col justify-center items-center gap-2 shadow-lg rounded-md p-2 relative ">
-        <div className="border border-gray-400 rounded-md py-1 px-2 bg-slate-100">Education Board</div>
-        <p className="border border-gray-400 py-1 px-2 text-sm rounded-sm w-full bg-slate-300">
-          Nurani Education Board, Chittagong
-        </p>
-        <Button className="mt-4">Add new education board</Button>
-      </div>
+        </CardContent>
 
-      <div className="flex flex-col justify-center items-center gap-2 shadow-lg rounded-md p-2 relative ">
-        <div className="border border-gray-400 rounded-md py-1 px-2 bg-slate-100">Class</div>
-        <div className="grid grid-cols-2 gap-x-8 gap-y-4">
+        <CardFooter>
+          <Button>Add new education board</Button>
+        </CardFooter>
+      </Card>
+
+      <Card className="flex flex-col justify-center items-center gap-2 shadow-lg rounded-md">
+        <CardHeader>
+          <CardTitle className="border border-gray-400 rounded-md py-1 px-2 bg-slate-100 text-md font-medium">
+            Class
+          </CardTitle>
+        </CardHeader>
+        <CardContent className="grid grid-cols-2 gap-x-8 gap-y-4">
           <p className="border border-gray-400 py-1 px-2 text-sm rounded-sm bg-slate-300">Class one</p>
           <p className="border border-gray-400 py-1 px-2 text-sm rounded-sm bg-slate-300">Class two</p>
           <p className="border border-gray-400 py-1 px-2 text-sm rounded-sm bg-slate-300">Class three</p>
+        </CardContent>
+
+        <CardFooter>
           <Button>Add new class</Button>
-        </div>
-      </div>
+        </CardFooter>
+      </Card>
     </div>
   );
 };
