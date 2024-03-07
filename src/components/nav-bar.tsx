ade1ff43f1b49125c8d5932b34602db1f1e50501
Sk. Zaber Ahmed <zaber0305@gmail.com>
2024-03-08 00:58:45 +0600

    Modify: made bottom navbar css changes

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -27,15 +27,18 @@ const navbarMenus = [
 const NavBar = () => {
   return (
     <nav
-      className="fixed bottom-0 left-0 right-0  bg-gray-200"
+      className="fixed bottom-0 left-0 z-50 w-full h-16 bg-gray-200 border-t border-gray-200 dark:bg-gray-700 dark:border-gray-600"
       style={{ boxShadow: "inset 0 0 10px rgba(0,0,0,0.1)" }}>
-      <ul className="grid grid-cols-4 justify-items-center divide-x divide-gray-400">
+      <ul className="grid h-full max-w-2xl grid-cols-4 mx-auto font-medium divide-x divide-gray-400">
         {navbarMenus.map((menu) => (
           <NavLink
             to={menu.path}
             key={menu.title}
-            className="px-[1.67rem] py-2 cursor-pointer hover:text-gray-800">
+            className="inline-flex flex-col items-center justify-center px-5 border-gray-200 border-x hover:bg-gray-50 dark:hover:bg-gray-800 group dark:border-gray-600">
             <li>{menu.icon}</li>
+            <span className="text-sm sr-only text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500">
+              {menu.title}
+            </span>
           </NavLink>
         ))}
       </ul>
