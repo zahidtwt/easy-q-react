2df684c5bc6fcdffaa49e3271990a05b4b2127b0
TKRchamak <tonmoykumarroy6@gmail.com>
2024-04-14 01:58:06 +0600

    Feat: use react-query to fetch data and create hooks modify the axios instance as so on

--- src/redux/slices/demo/demoApi.ts
+++ src/redux/slices/demo/demoApi.ts
@@ -1,21 +1,21 @@
-import baseApi from "@/redux/api/baseApi";
+// import baseApi from "@/redux/api/baseApi";
 
-const taskApi = baseApi.injectEndpoints({
-  endpoints: (builder) => ({
-    getUserData: builder.query({
-      query: () => "posts",
-      providesTags: ["posts"],
-    }),
+// const taskApi = baseApi.injectEndpoints({
+//   endpoints: (builder) => ({
+//     getUserData: builder.query({
+//       query: () => "posts",
+//       providesTags: ["posts"],
+//     }),
 
-    updateUserData: builder.mutation({
-      query: (post: string) => ({
-        url: "/posts",
-        method: "POST",
-        body: post,
-      }),
-      invalidatesTags: ["posts"],
-    }),
-  }),
-});
+//     updateUserData: builder.mutation({
+//       query: (post: string) => ({
+//         url: "/posts",
+//         method: "POST",
+//         body: post,
+//       }),
+//       invalidatesTags: ["posts"],
+//     }),
+//   }),
+// });
 
-export const { useGetUserDataQuery, useUpdateUserDataMutation } = taskApi;
+// export const { useGetUserDataQuery, useUpdateUserDataMutation } = taskApi;
