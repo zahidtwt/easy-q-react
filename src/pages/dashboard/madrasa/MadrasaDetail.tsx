325f40956047ec42c03d278e2184f5b65c2172db
Tonmoy Kumar Roy <tonmoy.roy@selisegroup.com>
2024-04-21 01:29:54 +0600

    Feat: [ADD] new page for madrasha detail and build design

--- src/pages/dashboard/madrasa/MadrasaDetail.tsx
+++ src/pages/dashboard/madrasa/MadrasaDetail.tsx
@@ -1,8 +1,200 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { useParams } from "react-router-dom";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Input } from "@/components/ui/input";
+import { Plus, X } from "lucide-react";
+
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import useGetMadrasaDetail from "./hooks/useGetMadrasaDetail";
+import { toast } from "sonner";
+
+const FormSchema = z.object({
+  email: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+  mobile: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+});
 
 const MadrasaDetail = () => {
-  const param = useParams();
-  return <div>{param.id}</div>;
+  const { id } = useParams();
+
+  const { isLoading } = useGetMadrasaDetail({ Id: id });
+
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      email: "",
+      mobile: "",
+    },
+  });
+
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    // toast({
+    //   title: "You submitted the following values:",
+    //   description: (
+    //     <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+    //       <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+    //     </pre>
+    //   ),
+    // });
+
+    toast.success(data.email);
+  }
+
+  if (isLoading) {
+    return <p> loading ...</p>;
+  }
+
+  return (
+    <div className="p-3">
+      <div className="grid grid-cols-12 gap-2 items-center mb-5">
+        <div className="col-span-3">
+          <Avatar className="size-[100%] rounded-sm">
+            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarFallback>{"Ja".toUpperCase()}</AvatarFallback>
+          </Avatar>
+        </div>
+        <div className="col-span-9 grid ">
+          <h3 className="text-center font-semibold text-xl mb-1">Jamia Islamia Rowjatul Ulum Madrasa</h3>
+          <small className="text-center text-gray-500">Hat Govindpur, Faridpur</small>
+        </div>
+      </div>
+
+      <div className="grid gap-4">
+        <div className="col-span-12">
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="w-full space-y-3">
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="abcd..@.mail.com"
+                        {...field}
+                      />
+                    </FormControl>
+                    {/* <FormDescription>This is your public display name.</FormDescription> */}
+                    <FormMessage>{form?.formState?.errors.email?.message}</FormMessage>
+                  </FormItem>
+                )}
+              />
+            </form>
+          </Form>
+        </div>
+
+        <div className="col-span-12">
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="w-full space-y-3">
+              <FormField
+                control={form.control}
+                name="mobile"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Mobile</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="01......"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage>{form?.formState?.errors.email?.message}</FormMessage>
+                  </FormItem>
+                )}
+              />
+            </form>
+          </Form>
+        </div>
+
+        <div className="col-span-12">
+          <div className="w-full flex justify-between items-center mb-2">
+            <p className="font-medium text-xl">Education Board</p>
+            <button className="flex justify-center align-middle px-2 py-1 bg-red-400 rounded-lg">
+              <Plus size={18} />
+              <small className="ml-1">New Board</small>
+            </button>
+          </div>
+
+          <div className="w-full border-2 border-spacing-1 border-gray-50 rounded-md min-h-10">
+            <div className="flex flex-wrap gap-3 p-2">
+              <div className="flex border-2 border-red-500 col-auto rounded-full p-1">
+                <p>Board Name Name Name</p>
+                <div className="rounded-full border-2 border-red-500 ml-1">
+                  <X size={20} />
+                </div>
+              </div>
+              <div className="flex border-2 border-red-500 col-auto rounded-full p-1">
+                <p>Board Name</p>
+                <div className="rounded-full border-2 border-red-500 ml-1">
+                  <X size={20} />
+                </div>
+              </div>
+              <div className="flex border-2 border-red-500 col-auto rounded-full p-1">
+                <p>Board Name</p>
+                <div className="rounded-full border-2 border-red-500 ml-1">
+                  <X size={20} />
+                </div>
+              </div>
+              <div className="flex border-2 border-red-500 col-auto rounded-full p-1">
+                <p>Board Name</p>
+                <div className="rounded-full border-2 border-red-500 ml-1">
+                  <X size={20} />
+                </div>
+              </div>
+              <div className="flex border-2 border-red-500 col-auto rounded-full p-1">
+                <p>Board Name</p>
+                <div className="rounded-full border-2 border-red-500 ml-1">
+                  <X size={20} />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-span-12">
+          <div className="w-full flex justify-between items-center mb-2">
+            <p className="font-medium text-xl">Classes</p>
+            <button className="flex justify-center align-middle px-2 py-1 bg-red-400 rounded-lg">
+              <Plus size={18} />
+              <small className="ml-1">New Class</small>
+            </button>
+          </div>
+          <div className="w-full border-2 border-spacing-1 border-gray-50 rounded-md min-h-10">
+            <div className="flex flex-wrap gap-3 p-2">
+              <div className="flex border-2 border-red-500 col-auto rounded-full p-1">
+                <p>Board Name</p>
+                <div className="rounded-full border-2 border-red-500 ml-1">
+                  <X size={20} />
+                </div>
+              </div>
+              <div className="flex border-2 border-red-500 col-auto rounded-full p-1">
+                <p>Board Name</p>
+                <div className="rounded-full border-2 border-red-500 ml-1">
+                  <X size={20} />
+                </div>
+              </div>
+              <div className="flex border-2 border-red-500 col-auto rounded-full p-1">
+                <p>Board Name</p>
+                <div className="rounded-full border-2 border-red-500 ml-1">
+                  <X size={20} />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MadrasaDetail;
---
+++ src/pages/dashboard/madrasa/hooks/useGetMadrasaDetail.ts
@@ -0,0 +1,45 @@
+// import { endpoints } from "@/configs/config";
+import axiosInstance from "@/utils/axios";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import Cookies from "js-cookie";
+
+// const fetchDataLakeData = async () => {
+//   return (
+//     await axiosInstance.get(endpoints.dashboard.getAllInstitute, {
+//       headers: {
+//         ...axiosInstance.defaults.headers.common, // Merge existing common headers
+//         Authorization: `Bearer ${Cookies.get("token")}`, // Add authorization header
+//       },
+//     })
+//   ).data;
+// };
+
+const apiCallingFunc = async () => {
+  return (
+    await axios.get("http://localhost:5173/mock-data/madrasas.json", {
+      headers: {
+        ...axiosInstance.defaults.headers.common, // Merge existing common headers
+        Authorization: `Bearer ${Cookies.get("token")}`, // Add authorization header
+      },
+    })
+  ).data;
+};
+
+const useGetMadrasaDetail = ({ Id, dataDecorator }: { Id?: string; dataDecorator?: (data: unknown) => void }) => {
+  return useQuery({
+    queryKey: ["dataLakeData"],
+    queryFn: () => apiCallingFunc(),
+    // enabled: !!Id,
+    staleTime: 10000,
+    select: (data) => {
+      if (dataDecorator) {
+        return dataDecorator(data);
+      }
+      return data;
+    },
+    enabled: !!Id,
+  });
+};
+
+export default useGetMadrasaDetail;
